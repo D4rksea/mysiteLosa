ac2e5de245e8fb3e9cb84958c009f8e5a87c460b
D4rksea <65219077+D4rksea@users.noreply.github.com>
2020-05-19 10:54:31 +0200

    cose

--- mysiteLosa/urls.py
+++ mysiteLosa/urls.py
@@ -3,10 +3,16 @@ from django.urls import include, path
 
 urlpatterns = [
     path('polls/', include('polls.urls')), #questi 2 bisgna inserirli nell'url dopo http://127.0.0.1:8000/ e ti fanno visualizzare varie scelte
-    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls), #questo serve per andare al pannello di amministrazione
 ]
 """
 python manage.py makemigrations polls dice a Django che ci sono stati dei cambiamenti all'inerno dei models
 ed i cambiamenti li troviamo nei file polls/migrations/0001_initial.py che possiamo anche editare a mano.
 python manage.py sqlmigrate polls 0001 crea l'SQL per creare la tabella con tutti i suoi attributi nel db
+"""
+
+"""
+il problema dell'harcoded è che cambia l'URL con tanti templates, una volta che l'hai definito in nel path() in poll.urls
+puoi rimuovere  una parte dell'url inserendo {% url %}.
+Namespacing URL names servono perché in un progetto Django ci possono essere varie app e così si possono differenziare i nomi degli URL e quindi non fare casino tra questi
 """
--- polls/views.py
+++ polls/views.py
@@ -1,13 +1,22 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from .models import Question
+from django.template import loader
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.") #questo fa visualizzare il messaggio Hello, world. You're at the polls index. 
-    # quando si digita l'indirizzo http://127.0.0.1:8000/polls/
+# def index(request):
+#     return HttpResponse("Hello, world. You're at the polls index.") #questo fa visualizzare il messaggio Hello, world. You're at the polls index. 
+#     # quando si digita l'indirizzo http://127.0.0.1:8000/polls/
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    # return HttpResponse("You're looking at question %s." % question_id)
+    # def detail(request, question_id):
+    #     try:
+    #         question = Question.objects.get(pk=question_id)
+    #     except Question.DoesNotExist:
+    #         raise Http404("Question does not exist")
+    #     return render(request, 'polls/detail.html', {'question': question})
+        question = get_object_or_404(Question, pk=question_id)
+        return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
     response = "You're looking at the results of question %s."
@@ -21,7 +30,6 @@ def index(request):
     # output = ', '.join([q.question_text for q in latest_question_list])
     # return HttpResponse(output)
     template = loader.get_template('polls/index.html')
-    context = {
-        'latest_question_list': latest_question_list,
-    }
-    return HttpResponse(template.render(context, request))
+    context = {'latest_question_list': latest_question_list}
+    # return HttpResponse(template.render(context, request))
+    return render(request, 'polls/index.html', context)

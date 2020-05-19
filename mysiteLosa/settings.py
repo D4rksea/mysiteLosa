9337f4b2e7e3f63a1c144ea73d0f29cd93edc3e7
D4rksea <65219077+D4rksea@users.noreply.github.com>
2020-05-19 10:15:32 +0200

    un po' di modifiche

--- mysiteLosa/settings.py
+++ mysiteLosa/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -68,7 +69,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'mysiteLosa.wsgi.application'
-
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
--- mysiteLosa/urls.py
+++ mysiteLosa/urls.py
@@ -1,21 +1,12 @@
-"""mysiteLosa URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
+    path('polls/', include('polls.urls')), #questi 2 bisgna inserirli nell'url dopo http://127.0.0.1:8000/ e ti fanno visualizzare varie scelte
     path('admin/', admin.site.urls),
 ]
+"""
+python manage.py makemigrations polls dice a Django che ci sono stati dei cambiamenti all'inerno dei models
+ed i cambiamenti li troviamo nei file polls/migrations/0001_initial.py che possiamo anche editare a mano.
+python manage.py sqlmigrate polls 0001 crea l'SQL per creare la tabella con tutti i suoi attributi nel db
+"""
